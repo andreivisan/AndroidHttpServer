8f8bd8592c013dd6e6c4f361869a0a7a63a3e00e
Andrei Visan <andrei.m.visan@gmail.com>
2015-07-31 11:02:08 +0200

    Removed unnecessary comments

--- app/src/main/java/server/http/android/androidhttpserver/MainActivity.java
+++ app/src/main/java/server/http/android/androidhttpserver/MainActivity.java
@@ -2,7 +2,6 @@ package server.http.android.androidhttpserver;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -23,23 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
